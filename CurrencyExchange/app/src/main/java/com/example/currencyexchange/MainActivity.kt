d79a578ff8d0f1242bf2bd8a6ab25cb3c5f5ea89
NoahKoh <kohyusheng@gmail.com>
2024-09-22 20:12:45 +0800

    added toolbar and dark theme

--- CurrencyExchange/app/src/main/java/com/example/currencyexchange/MainActivity.kt
+++ CurrencyExchange/app/src/main/java/com/example/currencyexchange/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.inputmethod.InputBinding
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.currencyexchange.databinding.ActivityMainBinding
@@ -22,6 +23,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Currency Exchange"
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
